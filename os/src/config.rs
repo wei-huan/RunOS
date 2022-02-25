89e309fecebea447eb3c9a4a9f30a2c13b1ad3e4
wei-huan <1520799054@qq.com>
2022-02-25 10:28:05 +0800

    小修改

--- os/src/config.rs
+++ os/src/config.rs
@@ -4,5 +4,3 @@
 pub const KERNEL_HEAP_SIZE: usize = 0x20_0000;
 // 内核堆最大页是2^4个页框，也就是buddy_system分配的最大块内存有64KB
 pub const HEAP_ALLOCATOR_MAX_ORDER: usize = 64;
-
-pub const MEMORY_END: usize = 0x80800000;
