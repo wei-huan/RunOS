b6c38b819554178018ef8a55fa66154d643c1bfc
wei-huan <1520799054@qq.com>
2022-03-03 14:28:02 +0800

    添加trap处理

---
+++ os/src/trap/trap.rs
@@ -0,0 +1,24 @@
+use riscv::register::{mtvec::TrapMode, scause, stval, stvec};
+
+pub fn entry_init() {
+    set_kernel_entry();
+}
+
+pub fn set_kernel_entry() {
+    unsafe {
+        stvec::write(trap_from_kernel as usize, TrapMode::Direct);
+    }
+}
+
+fn set_user_trap_entry() {
+    unsafe {
+        stvec::write(TRAMPOLINE as usize, TrapMode::Direct);
+    }
+}
+
+#[no_mangle]
+pub fn trap_from_kernel() -> ! {
+    use riscv::register::sepc;
+    println!("stval = {:#?}, sepc = 0x{:X}", stval::read(), sepc::read());
+    panic!("a trap {:?} from kernel!", scause::read().cause());
+}
