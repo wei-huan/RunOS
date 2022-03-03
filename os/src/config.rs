dd85531aff48b4a3c4b357802a90c12fdf421805
wei-huan <1520799054@qq.com>
2022-03-03 15:12:12 +0800

    修改bootloader位置

--- os/src/config.rs
+++ os/src/config.rs
@@ -10,3 +10,6 @@ pub const MEMORY_END: usize = 0x80800000;
 pub const PAGE_SIZE: usize = 4096;
 //
 pub const PAGE_SIZE_BITS: usize = 12;
+
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
+pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
--- user/src/lib.rs
+++ user/src/lib.rs
@@ -22,7 +22,7 @@ const USER_HEAP_SIZE: usize = 4096 * 8;
 static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
 
 #[global_allocator]
-static HEAP: LockedHeap::<64> = LockedHeap::empty();
+static HEAP: LockedHeap<64> = LockedHeap::empty();
 
 #[alloc_error_handler]
 pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
