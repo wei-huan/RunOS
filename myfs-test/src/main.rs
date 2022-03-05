d88f30d5d5c6554c8e944f47ac361f3e08ef3065
wei-huan <1520799054@qq.com>
2022-03-05 21:11:32 +0800

    文件系统添加

---
+++ myfs-test/src/main.rs
@@ -0,0 +1,10 @@
+use clap::{App, Arg};
+use easy_fs::{BlockDevice, EasyFileSystem};
+use std::fs::{read_dir, File, OpenOptions};
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::sync::Arc;
+use std::sync::Mutex;
+
+fn main() {
+    println!("Hello, world!");
+}
