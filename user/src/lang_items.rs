db999b10b73d0654e428e4bd80f2eea8192acb03
wei-huan <1520799054@qq.com>
2022-03-14 20:50:45 +0800

    user 文件夹

---
+++ user/src/lang_items.rs
@@ -0,0 +1,18 @@
+use super::{getpid, kill, SignalFlags};
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    kill(getpid() as usize, SignalFlags::SIGABRT.bits());
+    unreachable!()
+}
