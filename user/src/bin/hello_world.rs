5f7ecf884ac0eff8bdcee526c408b224b6042c22
wei-huan <1520799054@qq.com>
2022-03-20 21:11:17 +0800

    add test files

---
+++ user/src/bin/hello_world.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user;
+
+use user::get_time;
+
+#[no_mangle]
+pub fn main() -> i32 {
+    let start = get_time();
+    let mut end = get_time();
+    while end - start <= 100 {
+        // println!("sleep");
+        end = get_time();
+    }
+    // println!("Hello world from user mode program 0!");
+    0
+}
