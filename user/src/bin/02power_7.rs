0a1bd4941d052b7b9829fe8551589a6c09f9925e
wei-huan <1520799054@qq.com>
2022-03-21 20:38:14 +0800

    nothing

--- user/src/bin/02power_7.rs
+++ user/src/bin/02power_7.rs
@@ -24,6 +24,6 @@ unsafe fn main() -> i32 {
         }
     }
     // println!("{}^{} = {}(MOD {})", p, iter, S[cur], m);
-    // println!("Test power_7 OK!");
+    println!("Test power_7 OK!");
     0
 }
