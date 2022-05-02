897adb7f2596c05c591cc7a461727b5f9fabf333
RunOS <t3666736@outlook.com>
2022-05-02 15:21:15 +0800

    nothing

--- kernel/src/scheduler/mod.rs
+++ kernel/src/scheduler/mod.rs
@@ -78,27 +78,26 @@ pub fn add_initproc_into_fs() {
         }
     }
 
-    if let Some(_) = open("/", "user_shell", OpenFlags::RDONLY, DiskInodeType::File) {
+    if let Some(inode) = open("/", "user_shell", OpenFlags::RDONLY, DiskInodeType::File) {
         // println!("Already have init user_shell in FS");
-        // inode.delete();
+        inode.delete();
+    }
+    //Write apps user_shell to disk from mem
+    if let Some(inode) = open("/", "user_shell", OpenFlags::CREATE, DiskInodeType::File) {
+        // println!("Create user_shell ");
+        let mut data: Vec<&'static mut [u8]> = Vec::new();
+        data.push(unsafe {
+            core::slice::from_raw_parts_mut(
+                app_start[1] as *mut u8,
+                app_start[2] - app_start[1],
+            )
+        });
+        //data.extend_from_slice(  )
+        // println!("Start write user_shell ");
+        inode.write(UserBuffer::new(data));
+        // println!("User_shell OK");
     } else {
-        //Write apps user_shell to disk from mem
-        if let Some(inode) = open("/", "user_shell", OpenFlags::CREATE, DiskInodeType::File) {
-            // println!("Create user_shell ");
-            let mut data: Vec<&'static mut [u8]> = Vec::new();
-            data.push(unsafe {
-                core::slice::from_raw_parts_mut(
-                    app_start[1] as *mut u8,
-                    app_start[2] - app_start[1],
-                )
-            });
-            //data.extend_from_slice(  )
-            // println!("Start write user_shell ");
-            inode.write(UserBuffer::new(data));
-            // println!("User_shell OK");
-        } else {
-            panic!("user_shell create fail!");
-        }
+        panic!("user_shell create fail!");
     }
     // recycle pages
     let mut start_ppn = app_start[0] / PAGE_SIZE + 1;
